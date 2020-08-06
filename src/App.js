3b7255c1ec79860a359a6ad787630f5c9f1a12f7
Carl Ortutay <carlorty@gmail.com>
2020-08-06 09:49:40 -0400

    removed boilerplate. some styling.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+const api = {
+  key: "7f03fe2713e86cc7331098d06bc0954c",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
 
 function App() {
   return (
-    <div className="App">
-      EAT A FUCKING DICK!
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input type="text" className="search-bar" placeholder="Search..." />
+        </div>
+      </main>
     </div>
   );
 }
